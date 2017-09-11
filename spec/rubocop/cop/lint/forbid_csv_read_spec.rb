049ba4dad8a45c1530c5f3f624bfbe74a6e38613
kasei_san <kasei.san@gmail.com>
2017-09-11 16:04:09 +0900

    Add rspec context Does not use CSV.read

--- spec/rubocop/cop/lint/forbid_csv_read_spec.rb
+++ spec/rubocop/cop/lint/forbid_csv_read_spec.rb
@@ -3,14 +3,23 @@ require 'spec_helper'
 RSpec.describe RuboCop::Cop::Lint::ForbidCsvRead do
   subject(:cop) { described_class.new }
 
-  context 'raise' do
-    let(:source){'CSV.read("01.csv")'}
-    it '' do
-      inspect_source(cop, source)
+  context 'Use CSV.read' do
+    it 'Raise ForbidCsvRead' do
+      inspect_source(cop, 'CSV.read("01.csv")')
       expect(cop.messages)
         .to eq(['CSV.read wastes a lot of memory. Use CSV.foreach'])
       expect(cop.offenses.size).to eq(1)
       expect(cop.highlights).to eq(['CSV.read("01.csv")'])
+    end
+  end
+
+  context 'Does not use CSV.read' do
+    it 'Does not raise ForbidCsvRead' do
+      inspect_source(cop, 'CSV.open("01.csv")')
+      expect(cop.messages)
+        .to eq([])
+      expect(cop.offenses.size).to eq(0)
+      expect(cop.highlights).to eq([])
     end
   end
 end
