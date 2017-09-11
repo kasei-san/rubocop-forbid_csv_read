e8aa4f797b4a92ba5f6b41334333f272c5c5dcdc
kasei_san <kasei.san@gmail.com>
2017-09-11 11:50:23 +0900

    First commit

---
+++ lib/rubocop/forbid_csv_read.rb
@@ -0,0 +1,7 @@
+require "rubocop/forbid_csv_read/version"
+
+module Rubocop
+  module ForbidCsvRead
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/forbid_csv_read/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module ForbidCsvRead
+    VERSION = "0.1.0"
+  end
+end
