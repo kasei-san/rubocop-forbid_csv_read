9786c5af3fe0397df3441ec34b5f4059e512bb93
kasei_san <kasei.san@gmail.com>
2017-09-11 13:27:44 +0900

    rubocop/spec を参考にRubocop pluginに必要なコードを実装

--- lib/rubocop/forbid_csv_read.rb
+++ lib/rubocop/forbid_csv_read.rb
@@ -1,7 +1,10 @@
+require 'rubocop'
 require "rubocop/forbid_csv_read/version"
+require "rubocop/forbid_csv_read/inject"
 
-module Rubocop
-  module ForbidCsvRead
-    # Your code goes here...
-  end
-end
+PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
+CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
+RuboCop::ForbidCsvRead::Inject.defaults!
+
+# cops
+require 'rubocop/cop/lint/forbid_csv_read'
